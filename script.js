e853b5667ed3b01a6389035d585efe11d691d24a
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-04-06 16:36:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
+const list = document.getElementById("infi-list");
+let count = 0;
 
+function addItems(n) {
+  for (let i = 0; i < n; i++) {
+    const li = document.createElement("li");
+    count++;
+    li.textContent = `List Item ${count}`;
+    list.appendChild(li);
+  }
+}
+
+addItems(100);
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const fullHeight = document.documentElement.scrollHeight;
+
+  if (scrollTop + windowHeight >= fullHeight - 5) {
+    addItems(2);
+  }
+});
